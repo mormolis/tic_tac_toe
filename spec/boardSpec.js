aa0eaaf59d4c632965c041361733af86ff8f2207
Georgios <george.lamprakis@gmail.com>
2017-11-30 14:59:28 +0000

    fixes typo and converts anonymous functions to arrow notation

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,30 +1,35 @@
 
-describe("Player", function(){
+describe("Board", () => {
 
     var Board = require('./../lib/board.js');
     var board = new Board();
 
-    describe(".typeMarker", function(){
-        it ("should throw error if marker is different than x or o", function(){
+    describe(".inputMarker", () => {
+        it ("should throw error if marker is different than x or o", () => {
             expect(()=>{board.inputMarker(1,1,"f")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
-        it ("should throw error if column < 0", function(){
+
+        it ("should throw error if column < 0", () => {
             expect(()=>{board.inputMarker(1,-1,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
-        it ("should throw error if row < 0", function(){
+
+        it ("should throw error if row < 0", () => {
             expect(()=>{board.inputMarker(-1,1,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
-        it ("should throw error if row and column < 0 ", function(){
+
+        it ("should throw error if row and column < 0 ", () => {
             expect(()=>{board.inputMarker(-1,-1,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
 
-        it ("should throw error if column > 2", function(){
+        it ("should throw error if column > 2", () => {
             expect(()=>{board.inputMarker(1,3,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
-        it ("should throw error if row > 2", function(){
+        
+        it ("should throw error if row > 2", () => {
             expect(()=>{board.inputMarker(-1,3,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
-        it ("should throw error if row and column > 2 ", function(){
+        
+        it ("should throw error if row and column > 2 ", () => {
             expect(()=>{board.inputMarker(3,3,"x")}).toThrow(new RangeError("Marker out of border or incorect marker"));
         });
 
@@ -33,11 +38,9 @@ describe("Player", function(){
             expect(board._board[0][0]).toEqual("O");
         })
 
-        it ("should throw error if cell is already marked ", function(){
+        it ("should throw error if cell is already marked ", () => {
             board.inputMarker(1,1,"x");
             expect(()=>{board.inputMarker(1,1,"x")}).toThrow(new Error("Cell already marked"));
         });
-
     });
-    
 });
