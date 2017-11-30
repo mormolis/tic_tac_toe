772dd20674456c70587f317149c8f913279321f7
Georgios <george.lamprakis@gmail.com>
2017-11-30 20:43:09 +0000

    Adds logic for computer

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -15,15 +15,27 @@ describe("Player", () => {
             expect(player._preventLoss([" ", "O", "X"])).toEqual(-1);
         });
 
-
         it("should return the available cell if danger of loss", ()=>{
             expect(player._preventLoss(["O", " ", "O"])).toEqual(1);
-        })
+        });
+        
         it("should return the available cell if danger of loss", ()=>{
             expect(player._preventLoss([" ", "O", "O"])).toEqual(0);
-        })
+        });
+        
         it("should return the available cell if danger of loss", ()=>{
             expect(player._preventLoss(["O", "O", " "])).toEqual(2);
-        })
-    })
+        });
+    });
+
+    describe("._randomMove", ()=>{
+        var boardSpy = {
+            isCellEmpty : () => {return true}
+        }
+        it("should point to an empty cell", ()=>{
+            expect(player._randomMove(boardSpy).length).toEqual(2)
+        });
+    });
+
+
 });
