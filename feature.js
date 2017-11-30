72ec32be64cee81b23ef08018bc3affc3469098e
Georgios <george.lamprakis@gmail.com>
2017-11-30 22:13:13 +0000

    features bots playing together. not finished yet

--- feature.js
+++ feature.js
@@ -1,5 +1,9 @@
 var Board = require('./lib/board.js');
+var Game = require('./lib/game.js');
+var Player = require('./lib/player.js');
 
-var board = new Board();
 
-console.log(board.toString());
+var p1 = new Player("bot1", "X", "COMPUTER");
+var p2 = new Player("fakeHuman", "O", "COMPUTER");
+var game = new Game(p1,p2);
+game.start()
